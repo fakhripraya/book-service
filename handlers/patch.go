0ffbd10d56451d35b418a4d980386c22cf86eb5a
fakhripraya <fakhri@indomaret.co.id>
2021-01-16 18:21:51 +0700

    Update patch.go

--- handlers/patch.go
+++ handlers/patch.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/fakhripraya/book-service/config"
 	"github.com/fakhripraya/book-service/data"
@@ -10,8 +11,78 @@ import (
 	"gorm.io/gorm"
 )
 
-// ApprovalBookTransaction is a method to approve the book transaction info
-func (bookHandler *BookHandler) ApprovalBookTransaction(rw http.ResponseWriter, r *http.Request) {
+// OwnerApprovalBookTransaction is a method to approve the book transaction info by the owner
+func (bookHandler *BookHandler) OwnerApprovalBookTransaction(rw http.ResponseWriter, r *http.Request) {
+
+	// get the approval via context
+	approvalReq := r.Context().Value(KeyApproval{}).(*entities.ApprovalRoomBook)
+
+	// get the current user login
+	var currentUser *database.MasterUser
+	currentUser, err := bookHandler.book.GetCurrentUser(rw, r, bookHandler.store)
+	if err != nil {
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
+
+		return
+	}
+
+	// proceed to create the new approval with transaction scope
+	err = config.DB.Transaction(func(tx *gorm.DB) error {
+
+		// set variables
+		var targetBook database.DBTransactionRoomBook
+		var dbErr error
+
+		if dbErr := config.DB.Where("id = ?", approvalReq.BookID).First(&targetBook).Error; err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+
+			return dbErr
+		}
+
+		// Status 1 = approved by owner
+		// Status 3 = reject
+		if approvalReq.FlagApproval == true {
+			targetBook.Status = 1
+			targetBook.Modified = time.Now().Local()
+			targetBook.ModifiedBy = currentUser.Username
+		} else {
+			targetBook.Status = 3
+			targetBook.Modified = time.Now().Local()
+			targetBook.ModifiedBy = currentUser.Username
+		}
+
+		// update the room book
+		dbErr = config.DB.Save(targetBook).Error
+
+		if dbErr != nil {
+			return dbErr
+		}
+
+		return nil
+
+	})
+
+	// if transaction error
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
+
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	if approvalReq.FlagApproval == true {
+		data.ToJSON(&GenericError{Message: "Sukses Approve booking"}, rw)
+	} else {
+		data.ToJSON(&GenericError{Message: "Sukses Reject booking"}, rw)
+	}
+
+	return
+
+}
+
+// TenantApprovalBookTransaction is a method to approve the book transaction info by the tenant
+func (bookHandler *BookHandler) TenantApprovalBookTransaction(rw http.ResponseWriter, r *http.Request) {
 
 	// get the approval via context
 	approvalReq := r.Context().Value(KeyApproval{}).(*entities.ApprovalRoomBook)
@@ -50,12 +121,27 @@ func (bookHandler *BookHandler) ApprovalBookTransaction(rw http.ResponseWriter, 
 			return dbErr
 		}
 
-		// Status 1 = approved
+		// Status 2 = approved by user
+		// Status 3 = reject
 		if approvalReq.FlagApproval == true {
-			targetBook.Status = 1
+			targetBook.Status = 2
+			targetBook.Modified = time.Now().Local()
+			targetBook.ModifiedBy = currentUser.Username
+		} else {
+			targetBook.Status = 3
+			targetBook.Modified = time.Now().Local()
+			targetBook.ModifiedBy = currentUser.Username
+		}
+
+		// update the room book
+		dbErr = config.DB.Save(targetBook).Error
+
+		if dbErr != nil {
+			return dbErr
 		}
 
 		// Status 1 = approved
+		// TODO: tanyain lagi booknya gmn
 		if approvalReq.FlagApproval == true {
 			targetTransaction.PaidOff = targetTransaction.PaidOff + targetTransactionDetail.Payment
 		}
@@ -95,7 +181,12 @@ func (bookHandler *BookHandler) ApprovalBookTransaction(rw http.ResponseWriter, 
 	}
 
 	rw.WriteHeader(http.StatusOK)
-	data.ToJSON(&GenericError{Message: "Sukses request booking"}, rw)
+	if approvalReq.FlagApproval == true {
+		data.ToJSON(&GenericError{Message: "Sukses Approve booking"}, rw)
+	} else {
+		data.ToJSON(&GenericError{Message: "Sukses Reject booking"}, rw)
+	}
+
 	return
 
 }
